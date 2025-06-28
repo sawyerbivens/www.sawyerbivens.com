e5b5eaaf22070c523685ff1dd6a233bf3691b3b0
Sawyer Bivens <sawyer.codes.stuff@gmail.com>
2025-06-28 02:52:58 -0500

    Fix 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,35 +1,34 @@
-import { ChevronRight } from "feather-icons-react";
+"use client";
+
+import { Nav } from "@/components/nav";
+import { ChevronLeft } from "feather-icons-react";
 import Link from "next/link";
 
 export default function notFound() {
   return (
-    <main className="flex flex-col mx-auto min-h-screen">
-      <nav className="px-8 py-12 sticky top-0 bg-white/80 backdrop-blur-md">
-        <Link href="/" className="font-semibold">
-          Sawyer Bivens Photography
-        </Link>
-        <p className="text-sm">Sherwood, AR</p>
-      </nav>
-      <section className="px-8 flex flex-col gap-12 flex-1 pb-[140px]">
-        <header className="flex flex-col gap-12 my-auto">
-          <div className="flex flex-col gap-6">
-            <h1 className="text-3xl md:text-4xl font-bold text-center">
-              Page not found
-            </h1>
-            <p className="text-sm text-center">
-              If you think this is an error, please send me an email at{" "}
-              <a href="mailto:sawyerbivens.com">sawyerbivens06@gmail.com</a>
-            </p>
-          </div>
-          <Link
-            href="/"
-            className="group bg-black rounded-sm p-4 text-white cursor-pointer flex items-center flex-row gap-2 font-semibold justify-center"
+    <main className="flex flex-col lg:flex-row min-h-screen">
+      <Nav active="/contact" />
+      <div className="flex-1 p-12 max-w-lg w-full m-auto pb-[189px] lg:pb-12 flex flex-col gap-4 items-center justify-center">
+        <h1 className="text-3xl md:text-4xl font-bold text-center">
+          Page not found
+        </h1>
+        <p className="text-sm text-center">
+          If you think this is an error, please send me an email at{" "}
+          <a
+            href="mailto:sawyerbivens.com"
+            className="underline cursor-pointer"
           >
-            <span>Home</span>
-            <ChevronRight className="text-white h-4 w-4 group-hover:ml-1 duration-200" />
-          </Link>
-        </header>
-      </section>
+            sawyerbivens06@gmail.com
+          </a>
+        </p>
+        <Link
+          className="bg-black text-white p-2 cursor-pointer rounded-sm text-center flex flex-row items-center gap-2 justify-center hover:shadow-sm shadow-black/50 hover:shadow-black/50 duration-200 font-semibold w-full"
+          href="/"
+        >
+          <ChevronLeft className="h-4 w-4" />
+          <span>Back to All Photos</span>
+        </Link>
+      </div>
     </main>
   );
 }
